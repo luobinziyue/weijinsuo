e7552ec6347cbbe2742ea99c93ef0ae83f016e49
luodaxia <450910277@qq.com>
2018-09-12 09:04:30 +0800

    添加了新闻板块

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 $(function() {
     $('[data-toggle="tooltip"]').tooltip();
-    var ul = $('.nav-tabs');
+
+    var ul = $('.product .nav-tabs');
+    console.log(ul);
     var totalWidth = 0;
     var lis = ul.find('li');
     lis.each(function(index,value) {
         totalWidth += $(value).outerWidth(true);
-        console.log(totalWidth);
+        //console.log(totalWidth);
     })
     console.log(totalWidth);
     ul.width(totalWidth);
